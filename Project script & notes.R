44e440dd20e8618c45b7a96c88bcbf8494078a73
hannahdemler <hdemler2@illinois.edu>
2020-10-05 11:44:53 -0500

    Added code for the mean yields by year and by country

--- Project script & notes.R
+++ Project script & notes.R
@@ -14,3 +14,27 @@ dim(key_crop_yields)  #dimensions of the data frame, 14 columns 13075 rows
 
 summary(key_crop_yields) # summary of all variables
 
+## summary stats on the different yields all countries and years##
+sumStats<-function(X,...){
+  xbar<-mean(X,...)
+  sd<-sd(X,...)
+  q1<-quantile(X,p=0.25,...)
+  med<-median(X,...)
+  q3<-quantile(X,p=0.75,...)
+  c(mean =xbar, sd = sd, Q1 = q1, median = med, Q3 = q3)
+}
+apply(key_crop_yields[,-c(1:3)],2,FUN=sumStats, na.rm=T)
+
+key_crop_yields$Year<-as.factor(key_crop_yields$Year) ## changing Year variable to a factor
+
+##Mean yields by country##
+key_crop_yields %>%
+  group_by(Entity) %>%
+  summarise_if(is.double,mean, na.rm=T)
+
+##Mean yields by year##
+key_crop_yields %>%
+  group_by(Year) %>%
+  summarise_if(is.double,mean, na.rm=T)
+
+
